64bc8bd351aa8f920da281d6a690c70c5334417f
Julie <juliet@ksu.edu>
2026-08-27 16:09:36 -0500

    notes before lab3

---
+++ week2_aug31-sep4/lab3notes/monAug31/Chips.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Chips {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        /*
+            bags of chips are buy one get one free at the store.
+            Full price is $5.99 each.
+
+            Customers are limited to 10 bags total.
+
+            (is there any other input that would be invalid?)
+        */
+
+    }
+}
