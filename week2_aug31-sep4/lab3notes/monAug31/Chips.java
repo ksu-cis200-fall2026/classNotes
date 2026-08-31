2d7313141adbb071bfc42be0e870c2d0d579ceeb
Julie <juliet@ksu.edu>
2026-08-31 14:05:15 -0500

    after mon lab 3

--- week2_aug31-sep4/lab3notes/monAug31/Chips.java
+++ week2_aug31-sep4/lab3notes/monAug31/Chips.java
@@ -4,6 +4,32 @@ public class Chips {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter how many bags of chips: ");
+        int chips = s.nextInt();
+
+        //validate the input
+        if (chips <= 10 && chips > 0) {
+            //put my price calculation
+            double price;
+            if (chips % 2 == 0) {
+                //chips is even
+                price = chips * (5.99/2);
+            }
+            else {
+                //chips is odd
+                price = (chips/2)*5.99 + 5.99;
+            }
+            //Total price: $11.98
+            System.out.printf("Total price: $%.2f%n", price);
+        }
+        else {
+            System.out.println("Error: chips should be between 0-10");
+        }
+
+        //if buying even number, pair them all up
+        //otherwise, pair up the even portion plus full price for the extra
+
+        
         /*
             bags of chips are buy one get one free at the store.
             Full price is $5.99 each.
