526bf73a5d5a0c6c95e03596515f9e0b2f982836
Julie <juliet@ksu.edu>
2026-09-01 15:10:41 -0500

    notes before lab 4

---
+++ week2_aug31-sep4/lab4notes/thursSep3/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        //get user input number
+        System.out.print("Enter a whole number: ");
+        int num = s.nextInt();
+
+        //find the factorial of the input number
+
+        //5! = 5*4*3*2*1
+        //how to add input validation?
+    }
+}
