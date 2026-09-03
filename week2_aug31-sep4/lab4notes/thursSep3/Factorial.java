2d3a7a0199ef4393af9355cf51f81bbe6af4dc62
Julie <juliet@ksu.edu>
2026-09-03 13:46:37 -0500

    after thurs lab4

--- week2_aug31-sep4/lab4notes/thursSep3/Factorial.java
+++ week2_aug31-sep4/lab4notes/thursSep3/Factorial.java
@@ -1,3 +1,10 @@
+/**
+ * Project 2 finds ....
+ * 
+ * @author Julie Thornton
+ * @version Project 2
+ */
+
 import java.util.*;
 
 public class Factorial {
@@ -8,7 +15,25 @@ public class Factorial {
         System.out.print("Enter a whole number: ");
         int num = s.nextInt();
 
-        //find the factorial of the input number
+        //validate input
+        if (num >= 1) {
+            //find the factorial of the input number
+
+            int cur = num;
+            int product = 1;
+            while (cur >= 1) {
+                product = product*cur;
+                cur--;
+            }
+
+            //print: 5! = 120
+            System.out.printf("%d! = %d%n", num, product);
+        }
+        else {
+            System.out.println("Error - input must be a positive whole number");
+        }
+
+        
 
         //5! = 5*4*3*2*1
         //how to add input validation?
