d2f45dc76bc26b014b908d52a21972e8f3eb90cb
Julie <juliet@ksu.edu>
2026-09-14 14:14:07 -0500

    notes after mon sep 14 lab 6

--- week4_sep14-18/lab6notes/monSep14/DiceGame.java
+++ week4_sep14-18/lab6notes/monSep14/DiceGame.java
@@ -9,35 +9,44 @@ public class DiceGame {
         Scanner s = new Scanner(System.in);
         Random r = new Random();
 
-        int total = 0;
-        do {
-            //roll a dice
-            int roll = r.nextInt(6) + 1;
-            System.out.printf("You rolled %d%n", roll);
+        //introduce a high score variable
 
-            //did they roll a 1?
-            if (roll == 1) {
-                total = 0;
-                break;
+        do {
+            int total = 0;
+            do {
+                //roll a dice
+                int roll = r.nextInt(6) + 1;
+                System.out.printf("You rolled %d%n", roll);
+
+                //did they roll a 1?
+                if (roll == 1) {
+                    total = 0;
+                    break;
+                }
+                else {
+                    total = total + roll;
+                }
+
+                //if they still haven't won, do they want to go again?
+                if (total < 20) {
+                    System.out.printf("Game total: %d points%n", total);
+                    System.out.print("\nPress Enter to roll again: ");
+                    s.nextLine();
+                }
+            } while (total < 20);
+
+            //print end of game stuff
+            if (total >= 20) {
+                System.out.printf("\nFinal score: %d points. You win!%n", total);
             }
             else {
-                total = total + roll;
+                System.out.printf("\nFinal score: %d points. You lose.%n", total);
             }
 
-            //if they still haven't won, do they want to go again?
-            if (total < 20) {
-                System.out.printf("Game total: %d points%n", total);
-                System.out.print("\nPress Enter to roll again: ");
-                s.nextLine();
-            }
-        } while (total < 20);
+            //compare new score against high score, update if needed
 
-        //print end of game stuff
-        if (total >= 20) {
-            System.out.printf("\nFinal score: %d points. You win!%n", total);
-        }
-        else {
-            System.out.printf("\nFinal score: %d points. You lose.%n", total);
-        }
+            //ask if they want to go again
+
+        } while (they want to go again);
     }
 }
