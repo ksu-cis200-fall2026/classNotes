bebe43b2f7b1b31d8fd9fba4ad2b7dcec8194bf4
Julie <juliet@ksu.edu>
2026-09-15 14:25:40 -0500

    proj3 solution

---
+++ week5_sep21-25/tuesSep22/ArrayPractice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        /*
+            Get 6 numbers from the user, all in a line.
+
+            Print them in reverse.
+        */
+    }
+}
