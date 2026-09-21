f1d4b56b513607562406b4b0c19c0bf2ed53e1af
Julie <juliet@ksu.edu>
2026-09-21 14:03:34 -0500

    after lab 8 mon sep 21

--- week5_sep21-25/monSep21/ArrayPractice.java
+++ week5_sep21-25/monSep21/ArrayPractice.java
@@ -5,6 +5,29 @@ public class ArrayPractice {
         Scanner s = new Scanner(System.in);
 
         /*
+        int[] nums = new int[5];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = 2*(i+1);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        */
+
+       int[] vals = new int[6];
+       System.out.print("Enter 6 numbers: ");
+       for (int i = 0; i < vals.length; i++) {
+            vals[i] = s.nextInt();
+       }
+
+       System.out.println("Reversed:");
+       for (int i = vals.length-1; i >= 0; i--) {
+            System.out.println(vals[i]);
+       }
+
+        /*
             Get 6 numbers from the user, all in a line.
 
             Print them in reverse.
