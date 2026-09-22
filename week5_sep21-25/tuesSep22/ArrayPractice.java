3d0179cc67edff99ebdeb8e420a34de26e1f21dd
Julie <juliet@ksu.edu>
2026-09-22 13:40:05 -0500

    after lab 8 tues sep 22

--- week5_sep21-25/tuesSep22/ArrayPractice.java
+++ week5_sep21-25/tuesSep22/ArrayPractice.java
@@ -5,9 +5,33 @@ public class ArrayPractice {
         Scanner s = new Scanner(System.in);
 
         /*
-            Get 6 numbers from the user, all in a line.
+        int[] nums = new int[5];
 
-            Print them in reverse.
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = 2*(i+1);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        */
+
+       int[] ages = new int[6];
+       System.out.print("Enter ages: ");
+       for (int i = 0; i < ages.length; i++) {
+            ages[i] = s.nextInt();
+       }
+
+       System.out.println("Teenagers:");
+       for (int i = 0; i < ages.length; i++) {
+            if (ages[i] >= 13 && ages[i] < 20) {
+                System.out.println(ages[i]);
+            }
+       }
+
+        /*
+            Get 6 ages from the user, all in a line.
+
+            Print the ages that are teenagers
         */
     }
 }
